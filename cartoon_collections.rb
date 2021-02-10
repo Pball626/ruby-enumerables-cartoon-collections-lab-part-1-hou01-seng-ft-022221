90cee0cca10ad44548e8bcd3a6f44a750e80f97b
Jose Mendoza <pball626@gmail.com>
2021-02-10 00:19:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,17 @@
+require "pry"
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  hello = 0 
+  while array.each do "Hello!"
+  end
+  array
 end
+    
 
-def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
-end
+# def list_dwarves(array)
+#   # Use `each_with_index` to enumerate over the provided array
+#   #
+#   # Print a numbered list of each element
+# end
