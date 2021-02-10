a641c3407544ff177cb27be0120b94841babbbe1
Jose Mendoza <pball626@gmail.com>
2021-02-10 00:50:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  array.each_with_index  { |
+  array.each_with_index  { |"Hello"(0)(1)(2)"!"| }
   #do |element|
     text = "Hello Dopey!"
     puts text
